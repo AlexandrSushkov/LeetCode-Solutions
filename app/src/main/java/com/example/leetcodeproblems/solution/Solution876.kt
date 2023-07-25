4a9a7868a578744b2e8dffba849b718a3f11da72
Alexandr Sushkov <alexandr.sushkov.mail@gmail.com>
2023-07-25 11:31:12 +0300

    Day 21 Problem 876. Middle of the Linked List.

---
+++ app/src/main/java/com/example/leetcodeproblems/solution/Solution876.kt
@@ -0,0 +1,15 @@
+package com.example.leetcodeproblems.solution
+
+import com.example.leetcodeproblems.solution.utils.ListNode
+
+class Solution876(private val head: ListNode?) : Solution<ListNode?> {
+    override fun execute(): ListNode? {
+        var slow: ListNode? = ListNode(-1).apply { this.next = head }
+        var fast: ListNode? = slow
+        while (fast != null) {
+            slow = slow?.next
+            fast = fast.next?.next
+        }
+        return slow
+    }
+}
