35cf3a45bf362236112286eb69a1db412719dfed
Alexandr Sushkov <alexandr.sushkov.mail@gmail.com>
2023-07-31 18:38:23 +0300

    Day 29 Problem 203. Remove Linked List Elements.

---
+++ app/src/main/java/com/example/leetcodeproblems/solution/Solution203.kt
@@ -0,0 +1,19 @@
+package com.example.leetcodeproblems.solution
+
+import com.example.leetcodeproblems.solution.utils.ListNode
+
+
+class Solution203(private val head: ListNode?, private val `val`: Int) : Solution<ListNode?> {
+    override fun execute(): ListNode? {
+        if (head == null) return null
+        val dummy = ListNode(-1)
+        dummy.next = head
+        var prev = dummy
+        var curr = head
+        while (curr != null) {
+            if (curr.`val` == `val`) prev.next = curr.next else prev = curr
+            curr = curr.next
+        }
+        return dummy.next
+    }
+}
